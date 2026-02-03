b67a42ed95c9d1d784ca464d28920b9156766ca3
nyxtrace01 <nyxtrace2@gmail.com>
2026-02-03 12:30:49 +0000

    ggfjh

--- problem2_grades.py
+++ problem2_grades.py
@@ -14,3 +14,32 @@ for student in students:
 #CALCULE T AFFICHE 
 
 #la moyenne de la classe
+total_notes=0
+for student in students:
+    total_notes += student[1]       
+moyenne=total_notes/len(students)
+print("La moyenne de la classe est:",moyenne)
+#la note maximale 
+max_note=students[0][1]
+for student in students:
+    if student[1] > max_note:
+        max_note=student[1]
+print("La note maximale est:",max_note)
+#la note minimale   
+min_note=students[0][1]
+for student in students:
+    if student[1] < min_note:
+        min_note=student[1] 
+print("La note minimale est:",min_note)
+#la liste des etudiants admis
+etudiants_reussite=[]
+for student in students:
+    if student[1] >= 10:
+        etudiants_reussite.append(student[0])
+print("Les etudiants admis sont:",etudiants_reussite)
+#la liste des etudiants ajournees
+etudiants_echec=[]
+for student in students:
+    if student[1] < 10:
+        etudiants_echec.append(student[0])  
+print("Les etudiants ajournes sont:",etudiants_echec)
